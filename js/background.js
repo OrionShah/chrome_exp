5775e01aabcde4b685a6fc2e19a2b7e3be36c4c8
Andrey Shahov <andrey.shahov@webpp.ru>
2015-10-06 17:58:21 +0300

    Попытка сделать логин трелло(сделать по кнопке в background.js)

---
+++ js/background.js
@@ -0,0 +1,14 @@
+var authenticationSuccess = function() { console.log("Successful authentication"); };
+var authenticationFailure = function() { console.log("Failed authentication"); };
+Trello.authorize({
+  type: "redirect",
+  name: "Getting Started Application",
+  scope: {
+    read: true,
+    write: true },
+  expiration: "never",
+  authenticationSuccess,
+  authenticationFailure
+});
+
+console.log(Trello.token());

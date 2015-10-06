5442720dc157772f72f53630c1b2535dfc186f26
OrionShah <dronshv@yandex.ru>
2015-10-07 00:02:43 +0300

    Разбор этого долбанного плагина в нечто, что должно работать

---
+++ js/content.js
@@ -0,0 +1,19 @@
+var authenticationSuccess = function() { console.log("Successful authentication"); };
+var authenticationFailure = function() { console.log("Failed authentication"); };
+
+// Trello.authorize({
+//   type: "redirect",
+//   name: "Getting Started Application",
+//   scope: {
+//     read: true,
+//     write: true },
+//   expiration: "never",
+//   authenticationSuccess,
+//   authenticationFailure
+// });
+
+// console.log(Trello.token());
+
+$('#TrelloLogin').click(function() {
+	console.log('click');
+});
